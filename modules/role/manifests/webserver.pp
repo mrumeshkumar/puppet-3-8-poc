504b7589fe91a90a9737a33aad34a21c96d65492
mrumeshkumar <mrumeshkumar@hotmail.com>
2020-04-29 18:35:38 +0530

    updated ntp module

--- modules/role/manifests/webserver.pp
+++ modules/role/manifests/webserver.pp
@@ -4,8 +4,8 @@ class role::webserver
   class { 'motd':
     template => 'motd/web_server.erb',
   }
-    #class { '::ntp':
-    # servers => [ '2.2.2.2' ],
-    #}
+  class { 'ntp':
+     servers => [ '2.2.2.2' ],
+    }
 
 }
